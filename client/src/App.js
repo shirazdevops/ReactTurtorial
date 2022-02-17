beef73b9cdb7bc1a56e926c42687c59b11dc5d5e
shirazdevops <shirazdevops@gmail.com>
2022-02-16 21:31:03 -0500

    added name again

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
       <h1> SHIRAZ </h1>
+      <h1> SHIRAZ </h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
